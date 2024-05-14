29459d1f341e44c0db114edf92eeda900f32778c
yunlinIT <yunlinit@gmail.com>
2024-05-14 09:18:29 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/jwt_2024_05/Jwt202405Application.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_2024_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt202405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt202405Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/jwt_2024_05/Jwt202405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.jwt_2024_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt202405ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
